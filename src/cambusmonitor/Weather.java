e5a1b323997ba9265ec91743878d779cf580c69b
hyh0220 <alwaysaxl@hotmail.com>
2014-03-10 16:04:34 -0500

    3.10

--- src/cambusmonitor/Weather.java
+++ src/cambusmonitor/Weather.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -33,49 +32,56 @@ public class Weather extends Thread{
     public Weather(){
         
     }
+    @Override
     public void run(){
-        try {
-            Date d = new Date();
-            SimpleDateFormat fn = new SimpleDateFormat ("yyyy.MM.dd'weather'");
-            SimpleDateFormat tm = new SimpleDateFormat ("hhmmss");
-            time = tm.format(d);
-            filename = fn.format(d);
-            writer = new PrintWriter ("/Users/yanhaohu/Desktop/output/"+filename+".txt", "UTF-8");
-            while(true){
-                if (d.getHours() == 2){
-                    break;
-                }
-                URL JsonURL = null;
-                try {
-                    JsonURL = new URL("http://api.wunderground.com/api/4fd6e9b1874ba8a5/conditions/q/IA/IOWA_CITY.json");
-                } catch (MalformedURLException ex) {
-                    Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                try {
-                    InputStream Json = JsonURL.openStream();
-                    InputStreamReader J = new InputStreamReader(Json);
-                    JSONParser jsonParser = new JSONParser();
-                    JSONObject jsonObject = (JSONObject) jsonParser.parse(J);
-                    JSONObject current = (JSONObject) jsonObject.get("current_observation");
-                    double temperature = (double) current.get("temp_f");
-                    String weather = (String) current.get("weather");
-                    String visibility = (String) current.get("visibility_mi");
-                    String rain1h = (String) current.get("precip_1hr_in");
-                    String rainday = (String) current.get("precip_today_in");
-                    writer.println(time+","+temperature+","+weather+","+visibility+","+rain1h+","+rainday);
-                    
-                } catch (        IOException | ParseException ex) {
-                    Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                try {
-                    Thread.sleep(1200000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }//end while
-            writer.close();
-        } catch (FileNotFoundException | UnsupportedEncodingException ex) {
-            Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
+        while(true){
+            try {
+                Date d = new Date();
+                int Min=d.getMinutes();
+                SimpleDateFormat fn = new SimpleDateFormat ("yyyy.MM.dd'weather'");
+                SimpleDateFormat tm = new SimpleDateFormat ("hhmmss");
+                //get file name
+                filename = fn.format(d);
+                writer = new PrintWriter ("/Users/yanhaohu/Desktop/output/"+filename+".txt", "UTF-8");
+                //start printing weather information
+                while(true){
+                    try{
+                    Date day = new Date();
+                    time = tm.format(day);
+                    //stop and create new file
+                    if (day.getHours() == 2){
+                        break;
+                    }
+                    //test use
+                    if (day.getMinutes()-Min>=1){
+                        break;
+                    }
+                    // get URL from weather underground
+                        URL JsonURL = new URL("http://api.wunderground.com/api/4fd6e9b1874ba8a5/conditions/q/IA/IOWA_CITY.json");
+                        InputStream Json = JsonURL.openStream();
+                        InputStreamReader J = new InputStreamReader(Json);
+                        //Parse Json file
+                        JSONParser jsonParser = new JSONParser();
+                        JSONObject jsonObject = (JSONObject) jsonParser.parse(J);
+                        //getting information
+                        JSONObject current = (JSONObject) jsonObject.get("current_observation");
+                        double temperature = (double) current.get("temp_f");
+                        String weather = (String) current.get("weather");
+                        String visibility = (String) current.get("visibility_mi");
+                        String rain1h = (String) current.get("precip_1hr_in");
+                        String rainday = (String) current.get("precip_today_in");
+                        //printout information
+                        writer.println(time+","+temperature+","+weather+","+visibility+","+rain1h+","+rainday);
+
+                    } catch (        IOException | ParseException ex) {
+                        Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                        Thread.sleep(1200000);
+                }//end while
+                writer.close();
+            } catch (FileNotFoundException | UnsupportedEncodingException | InterruptedException ex) {
+                Logger.getLogger(Weather.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
--- src/cambusmonitor/getUpcomingBus.java
+++ src/cambusmonitor/getUpcomingBus.java
@@ -5,20 +5,18 @@
  */
 
 package cambusmonitor;
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Hashtable;
-import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,6 +25,7 @@ import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException; 
 /**
@@ -43,136 +42,133 @@ public class getUpcomingBus extends Thread{
         this.Agency=Agency;
     }
     
-    public void run()
-    {
-        Date day=new Date();
-        Calendar C = Calendar.getInstance();
-        
-        //current minute, for testing purpose
-        int Min=day.getMinutes();
-      
-        String filename = generateFilename();
-        //System.out.println(filename);
-        Map map = new HashMap();
-        Hashtable ht = new Hashtable();
-        Enumeration ID = ht.keys();
-        String id = "";
-        String location = "";
-        
-        //temp stores previous location
-        
-        PrintWriter writer;
-      
-        try{
-            writer = new PrintWriter ("/Users/yanhaohu/Desktop/output/"+filename+Bus+".txt", "UTF-8");
-            //continously fetch data
-            while(true){
-                Date d = new Date();
-                String Time = generateTime();
-                //for test purpose, only run for 1 minutes
-                if (d.getMinutes()-Min>=1){
-                    break;
-                }
-                //run as late as 2 am
-                int H = d.getHours();
-                if (H == 2){
-                    break;
-                }
-                //String output =Bus+",";
-                try {
-                    //get API xml file
-                    URL xmlURL=new URL("http://api.ebongo.org/buslocation?agency="+Agency+"&route="+Bus+"&api_key=xApBvduHbU8SRYvc74hJa7jO70Xx4XNO");
-                    InputStream xml = xmlURL.openStream();
-                    DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-                    Document doc = docBuilder.parse (xml);
-                    xml.close();
+    public void run(){
+        while (true){
+            Date day=new Date();
+          
+            //current minute, for testing purpose
+//            int Min=day.getMinutes();
 
-                    // normalize text representation
-                    doc.getDocumentElement ().normalize ();
-                    System.out.println ("Root element of the doc is " + doc.getDocumentElement().getNodeName());
-                    //output=output+doc.getDocumentElement().getNodeName()+",";
+            String filename = generateFilename();
+            Map map = new HashMap();
+            Hashtable ht = new Hashtable();
+            Enumeration ID = ht.keys();
+            String id = "";
+            String location = "";
 
+            //temp stores previous location
+            PrintWriter writer;
 
-                    NodeList listOfPredictions = doc.getElementsByTagName("bus");
-                    int totalPredictions = listOfPredictions.getLength();
-                    System.out.println("Total no of predictions : " + totalPredictions);
-
-                    //loop through all prediction (buses)
-                    for(int s=0; s<listOfPredictions.getLength() ; s++){
-                        String output =Bus+",";
-                        Node firstPredictionNode = listOfPredictions.item(s);
-                        if(firstPredictionNode.getNodeType() == Node.ELEMENT_NODE){
-
-                            Element firstPredictionElement = (Element)firstPredictionNode;
-
-                            //get id
-                            NodeList idList = firstPredictionElement.getElementsByTagName("id");
-                            Element idElement = (Element)idList.item(0);
-
-                            NodeList textIdList = idElement.getChildNodes();
-                            System.out.println("id : " + ((Node)textIdList.item(0)).getNodeValue().trim());
-                            output=output+((Node)textIdList.item(0)).getNodeValue().trim()+",";
-                            id =((Node)textIdList.item(0)).getNodeValue().trim();
-                            
-                            //System.out.println("output has only id : "+ output);
-
-                            //get lat
-                            NodeList latList = firstPredictionElement.getElementsByTagName("lat");
-                            Element latElement = (Element)latList.item(0);
-
-                            NodeList textLatList = latElement.getChildNodes();
-                            System.out.println("lat : " + ((Node)textLatList.item(0)).getNodeValue().trim());
-                            output=output+((Node)textLatList.item(0)).getNodeValue().trim()+",";
-                            location = ((Node)textLatList.item(0)).getNodeValue().trim();
-
-                            //get lng
-                            NodeList lngList = firstPredictionElement.getElementsByTagName("lng");
-                            Element lngElement = (Element)lngList.item(0);
-
-                            NodeList textLngList = lngElement.getChildNodes();
-                            System.out.println("lng : " + ((Node)textLngList.item(0)).getNodeValue().trim());
-                            output = output +((Node)textLngList.item(0)).getNodeValue().trim()+",";
-                            location += ((Node)textLngList.item(0)).getNodeValue().trim();
-                        
-                            //get heading
-                            NodeList headingList = firstPredictionElement.getElementsByTagName("heading");
-                            Element headingElement = (Element)headingList.item(0);
-
-                            NodeList textHeadingList = headingElement.getChildNodes();
-                            System.out.println("heading : " + ((Node)textHeadingList.item(0)).getNodeValue().trim());
-                            output = output + ((Node)textHeadingList.item(0)).getNodeValue().trim();
-                        }
-                        System.out.println("output: "+output);
-                        //if the location changes, wirte to file
-                        if (ht.get(id) == null){
-                            ht.put(id, "");
-                        }
-                        if (! ht.get(id).equals(location)){
-                            writer.println(Time+output);    
-                        }
-                        ht.put(id, location);
+            try{
+                writer = new PrintWriter ("/Users/yanhaohu/Desktop/output/"+filename+Bus+".txt", "UTF-8");
+                URL xmlURL=new URL("http://api.ebongo.org/buslocation?agency="+Agency+"&route="+Bus+"&api_key=xApBvduHbU8SRYvc74hJa7jO70Xx4XNO");
+                InputStream xml;
+                DocumentBuilderFactory docBuilderFactory;
+                docBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+                Document doc;
+                //continously fetch data
+                while(true){
+                    Date d = new Date();
+                    String Time = generateTime();
+                    //for test purpose, only run for 1 minutes
+//                    if (d.getMinutes()-Min>=1){
+//                        break;
+//                    }
+                    //run as late as 2 am
+                    int H = d.getHours();
+                    if (H == 2){
+                        break;
                     }
+                    try {
+                        //get API xml file
+                       xml = xmlURL.openStream();
+                       doc = docBuilder.parse (xml);
+                       xml.close();
+
+                        // normalize text representation
+                        doc.getDocumentElement ().normalize ();
+                        System.out.println ("Root element of the doc is " + doc.getDocumentElement().getNodeName());
+
+
+                        NodeList listOfPredictions = doc.getElementsByTagName("bus");
+                        int totalPredictions = listOfPredictions.getLength();
+                        System.out.println("Total no of predictions : " + totalPredictions);
+
+                        //loop through all prediction (buses)
+                        for(int s=0; s<listOfPredictions.getLength() ; s++){
+                            String output =Bus+",";
+                            Node firstPredictionNode = listOfPredictions.item(s);
+                            if(firstPredictionNode.getNodeType() == Node.ELEMENT_NODE){
+
+                                Element firstPredictionElement = (Element)firstPredictionNode;
+
+                                //get id
+                                NodeList idList = firstPredictionElement.getElementsByTagName("id");
+                                Element idElement = (Element)idList.item(0);
+
+                                NodeList textIdList = idElement.getChildNodes();
+                                System.out.println("id : " + ((Node)textIdList.item(0)).getNodeValue().trim());
+                                output=output+((Node)textIdList.item(0)).getNodeValue().trim()+",";
+                                id =((Node)textIdList.item(0)).getNodeValue().trim(); //get current id
+
+                                //get lat
+                                NodeList latList = firstPredictionElement.getElementsByTagName("lat");
+                                Element latElement = (Element)latList.item(0);
+
+                                NodeList textLatList = latElement.getChildNodes();
+                                System.out.println("lat : " + ((Node)textLatList.item(0)).getNodeValue().trim());
+                                output=output+((Node)textLatList.item(0)).getNodeValue().trim()+",";
+                                location = ((Node)textLatList.item(0)).getNodeValue().trim();
+
+                                //get lng
+                                NodeList lngList = firstPredictionElement.getElementsByTagName("lng");
+                                Element lngElement = (Element)lngList.item(0);
+
+                                NodeList textLngList = lngElement.getChildNodes();
+                                System.out.println("lng : " + ((Node)textLngList.item(0)).getNodeValue().trim());
+                                output = output +((Node)textLngList.item(0)).getNodeValue().trim()+",";
+                                location += ((Node)textLngList.item(0)).getNodeValue().trim(); //get current location
+
+                                //get heading
+                                NodeList headingList = firstPredictionElement.getElementsByTagName("heading");
+                                Element headingElement = (Element)headingList.item(0);
+
+                                NodeList textHeadingList = headingElement.getChildNodes();
+                                System.out.println("heading : " + ((Node)textHeadingList.item(0)).getNodeValue().trim());
+                                output = output + ((Node)textHeadingList.item(0)).getNodeValue().trim();
+                            }
+                            System.out.println("output: "+Time+output);
+                            //initialize hashtable
+                            if (ht.get(id) == null){
+                                ht.put(id, "");
+                            }
+                            //if the location changes, wirte to file
+                            if (! ht.get(id).equals(location)){
+                                writer.println(Time+output);    
+                            }
+                            //match id woth location
+                            ht.put(id, location);
+                        }
+                    }
+                    catch (SAXParseException err) {
+                        System.out.println ("** Parsing error" + ", line " + err.getLineNumber () + ", uri " + err.getSystemId ());
+                        System.out.println(" " + err.getMessage ());
+                    }
+                    catch (SAXException e) {
+                        Exception x = e.getException ();
+                    } catch (IOException ex) {
+                        Logger.getLogger(getUpcomingBus.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    Thread.sleep(1000);
                 }
-                catch (SAXParseException err) {
-                    System.out.println ("** Parsing error" + ", line " + err.getLineNumber () + ", uri " + err.getSystemId ());
-                    System.out.println(" " + err.getMessage ());
-                }
-                catch (SAXException e) {
-                    Exception x = e.getException ();
-                    ((x == null) ? e : x).printStackTrace ();
-                }
-                catch (Throwable t) {
-                    t.printStackTrace ();
-                }
-                Thread.sleep(1000);
+                writer.close();
             }
-            writer.close();
+            catch (FileNotFoundException | UnsupportedEncodingException e){}
+            catch (InterruptedException | MalformedURLException | ParserConfigurationException ex) {
+                Logger.getLogger(getUpcomingBus.class.getName()).log(Level.SEVERE, null, ex);
+            }   
         }
-        catch (FileNotFoundException | UnsupportedEncodingException e){}
-        catch (InterruptedException ex) {
-            Logger.getLogger(getUpcomingBus.class.getName()).log(Level.SEVERE, null, ex);
-        }   
     }
     //generate filename
     private String generateFilename(){
